29c6c369832fcc980e1099524a2171e584f29468
yeasirarafatpp <yeasira79@gmail.com>
2022-01-04 13:46:40 +0600

    add 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
+    const history = useHistory();
+    const backHome = () => {
+        history.push('/')
+    }
     return (
-        <div>
-            <h2>NotFound</h2>
+        <div id="notfound">
+            <div class="notfound">
+                <div class="notfound-404">
+                    <h1>Oops!</h1>
+                    <h2>404 - The Page can't be found</h2>
+                </div>
+                <button onClick={backHome} className='btn'>
+                    <a href=''>Go TO Homepage</a>
+                </button>
+            </div>
         </div>
     );
 };
